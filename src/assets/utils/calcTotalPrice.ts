ef42b1d327da6c32d2ad29f6b20ffeb3c25bcad3
Ira Bokhonska <bokhonska22241@gmail.com>
2023-10-19 16:41:06 +0300

    some corrects in cart

---
+++ src/assets/utils/calcTotalPrice.ts
@@ -0,0 +1,8 @@
+import React from "react";
+import { CartItemType } from "../../redux/slices/cartSlice";
+
+export const calcTotalPrice = (items: CartItemType[]) => {
+  return items.reduce((sum, obj) => {
+    return obj.price * obj.count + sum;
+  }, 0);
+};
---
+++ src/assets/utils/getCartFromLS.ts
@@ -0,0 +1,13 @@
+import React from "react";
+import { calcTotalPrice } from "./calcTotalPrice";
+
+export const getCartFromLS = () => {
+  const data = localStorage.getItem("cart");
+  const items = data ? JSON.parse(data) : [];
+  const totalPrice = calcTotalPrice(items);
+
+  return {
+    items,
+    totalPrice,
+  };
+};
